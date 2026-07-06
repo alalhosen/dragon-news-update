4a78c63986b1ba29c45a77b2308b25f65304e80a
Alal Hosen <alalhosen1992620120@gmail.com>
2026-07-06 23:53:09 +0600

    import logo & set in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from "../assets/logo.png"
 
 const Header = () => {
     return (
         <div>
+            <img src={logo} alt="" />
             <h2>Header</h2>
         </div>
     );
