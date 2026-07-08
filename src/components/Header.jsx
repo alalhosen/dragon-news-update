52c2d9ae80150f52db30a6245d92223dec20e9a7
Alal Hosen <alalhosen1992620120@gmail.com>
2026-07-08 23:53:26 +0600

    install date-fns & set date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from "../assets/logo.png"
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3'>
-            <img className='w- [350px]' src={logo} alt="" />
+            <img className='w- [400px]' src={logo} alt="" />
             <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p>{format(new Date(), "EEEE,")}</p>
         </div>
     );
 };
