9e37bfe571418bc87d7cae368f9164a88bbaf568
Alal Hosen <alalhosen1992620120@gmail.com>
2026-07-18 23:47:45 +0600

    marquee problem

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-// import Marquee from "react-fast-marquee";
+import Marquee from "react-fast-marquee";
 
 const LatestNews = () => {
   return (
-    <div className="flex items-center gap-5 bg-base-200 p-3">   
-      <p className="text=base-100 bg-secondary px-3 py-2">Latest News</p>
-
-      {/* <Marquee> */}
-      <p className="font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, non.</p>
-      {/* </Marquee> */}
+    <div className="flex items-center gap-5 bg-base-200 p-3">
+      <p className="text-base-100 bg-secondary px-3 py-2">Latest News</p>
+      <Marquee>
+        <p className="font-bold">
+          Lorem ipsum dolor sit amet consectetur adipisicing el
+        </p>
+      </Marquee>
     </div>
   );
 };
 
 export default LatestNews;
-    
