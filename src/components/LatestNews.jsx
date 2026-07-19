8db403745f9db7cf0c1f4488f0add5432b817580
Alal Hosen <alalhosen1992620120@gmail.com>
2026-07-19 23:51:47 +0600

    stop marquee

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,17 +1,86 @@
 import React from "react";
-import Marquee from "react-fast-marquee";
+// import Marquee from "react-fast-marquee";
 
 const LatestNews = () => {
   return (
     <div className="flex items-center gap-5 bg-base-200 p-3">
       <p className="text-base-100 bg-secondary px-3 py-2">Latest News</p>
-      <Marquee>
+      {/* <Marquee> */}
         <p className="font-bold">
           Lorem ipsum dolor sit amet consectetur adipisicing el
         </p>
-      </Marquee>
+      {/* </Marquee> */}
     </div>
   );
 };
 
 export default LatestNews;
+
+// ...................
+
+// import React from "react";
+// import Marquee from "react-fast-marquee";
+
+// console.log("Marquee:", Marquee);
+
+// const LatestNews = () => {
+//   return (
+//     <div>
+//       <h1>Latest News</h1>
+//     </div>
+//   );
+// };
+
+// export default LatestNews;
+
+// """""""""""""""""""
+
+// import React from "react";
+// import Marquee from "react-fast-marquee";
+
+// console.log(Marquee);
+
+// const LatestNews = () => {
+//   return (
+//     <div className="flex items-center gap-5 bg-base-200 p-3">
+//       <p className="text-base-100 bg-secondary px-3 py-2">
+//         Latest News
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default LatestNews;
+// /??????????????
+
+// import React from "react";
+// import Marquee from "react-fast-marquee";
+
+// console.log("Marquee =", Marquee);
+
+// const LatestNews = () => {
+//   return (
+//     <div>
+//       <h1>Latest News</h1>
+//     </div>
+//   );
+// };
+
+// export default LatestNews;
+
+
+
+// ...................
+
+// import React from "react";
+// import Marquee from "react-fast-marquee";
+
+// const LatestNews = () => {
+//   return (
+//     <Marquee>
+//       Hello World
+//     </Marquee>
+//   );
+// };
+
+// export default LatestNews;
