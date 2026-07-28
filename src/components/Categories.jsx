fdc5f3f74e74775b3a2b7d6f94bfaecb0abeb4ed
Alal Hosen <alalhosen1992620120@gmail.com>
2026-07-28 23:44:36 +0600

    some change & add

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Categories = () => {
     return (
         <div>
-            <h2 className="font-bold">All Categories    </h2>
+            <h2 className="font-bold">All Categories</h2>
         </div>
     );
 };
