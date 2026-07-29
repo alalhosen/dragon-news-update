9f775d30debd9ebe5d88f7c386f21226f449d2a2
Alal Hosen <alalhosen1992620120@gmail.com>
2026-07-29 23:51:32 +0600

    set loading in leftAside component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { use } from "react";
+const categoryPromise = fetch("/categories.json").then((res) => res.json());
 
 const Categories = () => {
-    return (
-        <div>
-            <h2 className="font-bold">All Categories</h2>
-        </div>
-    );
+  const categories = use(categoryPromise);
+  return (
+    <div>
+      <h2 className="font-bold">All Categories</h2>
+    </div>
+  );
 };
 
 export default Categories;
+ 
