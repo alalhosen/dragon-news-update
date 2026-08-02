eb6784a38ca9a5f1dc6838bd88f64a36268252f5
Alal Hosen <alalhosen1992620120@gmail.com>
2026-08-02 22:38:35 +0600

    problem sloving is successful & news title dynamic set

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,16 @@
 import React, { use } from "react";
+import { NavLink } from "react-router";
 const categoryPromise = fetch("/categories.json").then((res) => res.json());
 const Categories = () => {
   const categories = use(categoryPromise);
   return (
     <div>
       <h2 className="font-bold">All Categories ({categories.length})</h2>
+      <div className="grid grid-cols-1 mt-5 gap-5">
+        {categories.map((category) => (
+          <NavLink key={category.id} className={"btn bg-base-100"}>{category.name}</NavLink>
+        ))}
+      </div>
     </div>
   );
 };
