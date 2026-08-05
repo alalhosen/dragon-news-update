2f9e103391673dcbdc3c853f231084484df1d300
Alal Hosen <alalhosen1992620120@gmail.com>
2026-08-05 23:55:17 +0600

    use loop in categoryNews component

--- src/components/pages/CategoryNews.jsx
+++ src/components/pages/CategoryNews.jsx
@@ -9,10 +9,19 @@ const CategoryNews = () => {
   // console.log(id, data);
 
   useEffect(() => {
-    const filteredNews = data.filter((news) => news.category_id == id);
-    console.log(filteredNews);
-
-    setCategoryNews(filteredNews);
+    if (id == "0") {
+      setCategoryNews(data);
+      return;
+    } else if (id == "1") {
+      const filteredNews = data.filter(
+        (news) => news.others.is_today_pick == true,
+      );
+      setCategoryNews(filteredNews);
+    } else {
+      const filteredNews = data.filter((news) => news.category_id == id);
+      console.log(filteredNews);
+      setCategoryNews(filteredNews);
+    }
   }, [data, id]);
 
   return (
